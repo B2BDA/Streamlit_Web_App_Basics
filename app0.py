c607dcc8de0c769c6240d7b3cfe58f18d0648446
B2BDA <bishwarup1429@gmail.com>
2020-11-07 17:57:02 +0530

    Streamlit basics

--- app0.py
+++ app0.py
@@ -84,3 +84,60 @@ firstName = st.text_input("Enter First Name","Type Here....")
 
 if st.button("Sumbit"):
     st.success("Bonjour %s"%firstName)
+    
+# text area
+message = st.text_area("Enter Gmail ID","Type Here....")
+
+if st.button("Sumbit1"):
+    st.success("Bonjour %s"%message)
+    
+# Date input
+from datetime import datetime, time
+date = st.date_input("Today is " , datetime.now())
+
+# Time Input
+the_time = st.time_input("Now is", time())
+
+# Displaying JSON
+st.text("Displaying JSON")
+st.json({"Name":"B2B","Age":"24"})
+
+# Display Raw Codes with comments
+with st.echo():
+    # Importing Libs
+    import pandas as pd
+    import numpy as np
+    
+# Progress Bar
+bar = st.progress(0)
+for p in range(1,10):
+    bar.progress(p+1)
+    
+# Spinner
+import time
+with st.spinner("Wait..."):
+    time.sleep(5)
+st.success("Done")
+
+# Baloons (Kids stuff)
+st.balloons()
+
+# Side Bar
+st.sidebar.header("About")
+st.sidebar.text("Nothing to see here. BYE!!!")
+
+# Run a function
+@st.cache # this will make the code run faster due to caching
+def func(n = 100):
+    return n+10
+    
+st.write(func())
+
+# # Plot
+# st.pyplot()
+
+# # DataFrames
+# st.dataframes()
+
+# # Tables
+# st.table(df)
